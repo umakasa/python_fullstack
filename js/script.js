78f0a0ec9713575ee946536d34128d5751486a97
umakasa <umakasa916@gmail.com>
2026-07-28 10:40:39 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to NRIIT Learning Management System");
+let heading = document.getElementById("welcome");
+heading.innerHTML = "Welcome Future Software Engineers"
+console.log("heading element: ", heading)
---
+++ js/test.js
@@ -0,0 +1,36 @@
+console.log("Hello, world!")
+let student_name = "Uma devi"
+console.log("Student name is: " + student_name)
+let student_age = 20
+let course_name = "Python Full stack"
+console.log("Student age is: " + student_age, "Course name is: " + course_name)
+let fee = 10000
+let discount = 10
+let discounted_fee = fee - (fee * discount / 100)
+console.log("Discounted fee is: " + discounted_fee)
+let age=18
+if(age>=18){
+    console.log("Eligible for admission")   
+}
+else{
+    console.log("Not eligible for admission")
+}
+for(let i=1; i<=5; i++){
+    console.log("Iteration number:" + i)
+}
+student = {
+
+}
+const pi = 3.14
+console.log("value of pi is: " + pi)
+let student = {
+    name: "Uma devi",
+    age:20,
+    course: "python Full Stack",
+    fee: 10000
+}
+console.log("Student details: ", student)
+function greetstudent(name){
+    console.log("Hello " + name + ", Welcome to NRIIT Learning Management System")
+}
+greetstudent(student_name)

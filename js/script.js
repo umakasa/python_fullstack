bcd2ae35dafd37102ecc2fbfbd04c90798c36ca5
umakasa <umakasa916@gmail.com>
2026-07-29 10:06:00 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 alert("Welcome to NRIIT Learning Management System");
 let heading = document.getElementById("welcome");
 heading.innerHTML = "Welcome Future Software Engineers"
-console.log("heading element: ", heading)
+console.log("Heading element: ", heading)
+let msg = document.getElementById("message");
+msg.innerHTML = "Javascript is fun"
+console.log("Message element: ", msg)
+function showmessage() {
+    alert("Welcome to NRIIT Learning Management System")
+}
+function changeHeading() {
+    document.getElementById("welcome").innerHTML = "Welcome Python Fullstack Developers"}
+let heading1=document.querySelector("#welcome");
+console.log("Heading element: ", heading1)
+let button = document.getElementById("btnGreeting");
+button.addEventListener("click", function() {
+    alert("Welcome to javascript Event Handling");
+});

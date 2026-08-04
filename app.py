d52f0a05bee76b96612fd6a3730b0a21c54c5e5b
umakasa <umakasa916@gmail.com>
2026-08-04 10:01:01 +0000

    foder

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
 
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return  render_template("index.html")
+
+@app.route("/about")
+def about():  
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)

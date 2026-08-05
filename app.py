f48b44ed4e5cf5b74572202babe5a096f20bdee9
umakasa <umakasa916@gmail.com>
2026-08-05 10:43:24 +0000

    css html and app.py update

--- app.py
+++ app.py
@@ -22,16 +22,35 @@ def courses():
 def trainers():
     return render_template("trainers.html")
 
-@app.route('/register',method=[POST,"GET"])
+@app.route('/register',methods=["POST","GET"])
 def register():
-   if request.method=="POST"
-   name=request.form["name"]
-   email=request.form["email"]
-   password=request.form["password"]
-   dob=request.form["dob"]
-   gender=requst.form["gender"] 
-   course=requst.form["course"]  
-   return render_template("register,html") 
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"]
+        return render_template("register.html")
+    return render_template("register.html")
+
+@app.route('/login',methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return render_template("login.html")
+    return render_template ("login.html")
+
+@app.route('/api/register', methods=["POST"])
+def api_register():
+    data = request.get_json()
+    email = data.get("email")
+
+    if email in users_db:
+        return jsonify({"status": "error", "message": "User already exists with this email"}), 400
+
+    users_db[email] = data
+    return jsonify({"status": "sucess", "message": "Registration sucessful!"})
+
 
 
 if __name__ == '__main__':
